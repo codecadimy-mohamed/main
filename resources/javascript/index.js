d47e94ce87251c12112f32c13c856dc20ea11a14
mohamed ouanjine <email.for.tv.elhajji@gmail.com>
2023-05-10 16:53:05 +0200

    add necesry code to make the face

---
+++ resources/javascript/index.js
@@ -0,0 +1,27 @@
+const mainParagraph = document.getElementById('main-paragraph');
+const eyesEreaR = document.getElementById('eyes-area-r');
+const eyesEreaL = document.getElementById('eyes-area-l');
+const eyeCercleR = document.getElementById('eye-cercle-r')
+const eyeCercleL = document.getElementById('eye-cercle-l')
+const mouth = document.getElementById('mouth');
+// start events and functions
+const changeColor = () => {
+  eyesEreaR.style.backgroundColor = 'red';
+  eyesEreaL.style.backgroundColor = 'red';
+  eyeCercleR.style.top = '5%';
+  eyeCercleR.style.backgroundColor = '5%';
+  eyeCercleL.style.top = '5%';
+  mouth.style.borderColor = 'beige beige transparent'
+}
+mainParagraph.addEventListener('mouseover', changeColor);
+
+const returnColor = () => {
+  eyesEreaR.style.backgroundColor = '';
+  eyesEreaL.style.backgroundColor = '';
+  eyeCercleR.style.top = '';
+  eyeCercleR.style.backgroundColor = '';
+  eyeCercleL.style.top = '';
+  mouth.style.borderColor = ''
+
+}
+mainParagraph.addEventListener('mouseout', returnColor);
