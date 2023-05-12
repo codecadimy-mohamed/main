a0a1392853c56aefd66a6e8551e5760e3a3bf802
mohamed ouanjine <email.for.tv.elhajji@gmail.com>
2023-05-12 16:54:31 +0200

    add the greeting for face

--- resources/javascript/index.js
+++ resources/javascript/index.js
@@ -1,18 +1,19 @@
 const mainParagraph = document.getElementById('main-paragraph');
 const eyesEreaR = document.getElementById('eyes-area-r');
 const eyesEreaL = document.getElementById('eyes-area-l');
-const eyeCercleR = document.getElementById('eye-cercle-r')
-const eyeCercleL = document.getElementById('eye-cercle-l')
-const mouth = document.getElementById('mouth');
-const thoughtArea = document.getElementById('thought-bubble')
+const eyeCercleR = document.getElementById('eye-cercle-r');
+const eyeCercleL = document.getElementById('eye-cercle-l');
+const face = document.getElementById('about-me-face');
+const mouth = document.getElementById('mouth')
+
 // start events and functions
 const changeColor = () => {
-  eyesEreaR.style.backgroundColor = 'red';
-  eyesEreaL.style.backgroundColor = 'red';
+  eyesEreaR.style.backgroundColor = 'lightgreen';
+  eyesEreaL.style.backgroundColor = 'lightblue';
   eyeCercleR.style.top = '5%';
   eyeCercleR.style.backgroundColor = '5%';
   eyeCercleL.style.top = '5%';
-  mouth.style.borderColor = 'beige beige transparent';
+  mouth.style.borderRadius = '20px';
 
 }
 mainParagraph.addEventListener('mouseover', changeColor);
@@ -23,16 +24,25 @@ const returnColor = () => {
   eyeCercleR.style.top = '';
   eyeCercleR.style.backgroundColor = '';
   eyeCercleL.style.top = '';
-  mouth.style.borderColor = ''
+  mouth.style.borderRadius = ''
 
 }
 mainParagraph.addEventListener('mouseout', returnColor);
 
 // mouth interactions
-const thoughtRandomText = [];
-mouth.onmouseover = function() {
+const thoughtArea = document.getElementById('thought-bubble')
+const greetings = {
+  english: ["Hello", "Hi", "Hey", "What's up","مرحبا", "أهلا","Hola", "Buenas", "Que tal","Bonjour", "Salut", "Coucou"]
+};
+
+face.onmouseover = function () {
   thoughtArea.style.visibility = 'visible';
+  const gitGreet = greetings.english[Math.floor(Math.random() * greetings.english.length)];
+  const thoughtText = document.getElementById('face-greeting')
+  thoughtText.innerHTML = gitGreet;
+  mouth.style.borderRadius = '20px';
 }
-mouth.onmouseout = function() {
+face.onmouseout = function () {
   thoughtArea.style.visibility = 'hidden';
+  mouth.style.borderRadius = '';
 }
